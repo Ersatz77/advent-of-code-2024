3e88206dc9979182054c040a9fed2d5f367f4dc2
Ersatz <cm12mc@gmail.com>
2024-12-22 03:36:56 -0800

    Optimized solution for day 22

--- src/solutions/day_22/day_22.cpp
+++ src/solutions/day_22/day_22.cpp
@@ -44,47 +44,6 @@ namespace aoc
         return secret;
     }
 
-    static MarketSequenceMap find_sequences(const std::int64_t initial_secret)
-    {
-        std::int64_t secret = initial_secret;
-        std::vector<std::int64_t> prices({ initial_secret % 10 });
-        std::vector<std::int64_t> changes;
-        for (std::size_t i = 0; i < 2000; ++i)
-        {
-            secret = next_secret(secret);
-
-            const std::int64_t current_price = secret % 10;
-            const std::int64_t current_change = current_price - prices.back();
-
-            prices.push_back(current_price);
-            changes.push_back(current_change);
-        }
-
-        MarketSequenceMap sequences;
-        for (std::size_t i = 0; i < changes.size() - 4; ++i)
-        {
-            const std::array<std::int64_t, 4> sequence = { changes[i], changes[i + 1], changes[i + 2] , changes[i + 3] };
-            const std::int64_t price = prices[i + 4];
-
-            if (!sequences.contains(sequence))
-            {
-                sequences[sequence] = price;
-            }
-        }
-
-        return sequences;
-    }
-
-    static std::int64_t sell_information(const MarketSequenceMap& sequences, const MarketSequence& sequence)
-    {
-        if (sequences.contains(sequence))
-        {
-            return sequences.at(sequence);
-        }
-
-        return 0;
-    }
-
     std::string Day22::part_1(const std::filesystem::path& input_root) const
     {
         const auto initial_secrets = parse_input(input_root / "day_22.txt");
@@ -108,30 +67,44 @@ namespace aoc
     {
         const auto initial_secrets = parse_input(input_root / "day_22.txt");
 
-        // Find every sequence and also create a set of unique sequences
-        std::vector<MarketSequenceMap> all_sequences;
-        std::unordered_set<MarketSequence> unique_sequences;
+        std::unordered_map<std::array<std::int64_t, 4>, std::int64_t> bananas;
         for (const auto initial_secret : initial_secrets)
         {
-            const auto sequences = find_sequences(initial_secret);
-            all_sequences.push_back(sequences);
-            for (const auto& [k, _] : sequences)
+            // Generate the prices and changes between prices
+            std::int64_t secret = initial_secret;
+            std::vector<std::int64_t> prices({ initial_secret % 10 });
+            std::vector<std::int64_t> changes;
+            for (std::size_t i = 0; i < 2000; ++i)
             {
-                unique_sequences.insert(k);
+                secret = next_secret(secret);
+
+                const std::int64_t current_price = secret % 10;
+                const std::int64_t current_change = current_price - prices.back();
+
+                prices.push_back(current_price);
+                changes.push_back(current_change);
+            }
+
+            // Add the prices to the sequence map
+            std::unordered_set<std::array<std::int64_t, 4>> seen;
+            for (std::size_t i = 0; i < changes.size() - 4; ++i)
+            {
+                const std::array<std::int64_t, 4> sequence = { changes[i], changes[i + 1], changes[i + 2] , changes[i + 3] };
+                const std::int64_t price = prices[i + 4];
+
+                if (!seen.contains(sequence))
+                {
+                    bananas[sequence] += price;
+                    seen.insert(sequence);
+                }
             }
         }
 
-        // Figure out which unique sequence gives the most bananas
+        // Find the most amount of bananas in the sequence map
         std::int64_t most_bananas = std::numeric_limits<std::int64_t>::min();
-        for (const auto& sequence : unique_sequences)
+        for (const auto& [_, v] : bananas)
         {
-            std::int64_t bananas = 0;
-            for (const auto& sequences : all_sequences)
-            {
-                bananas += sell_information(sequences, sequence);
-            }
-
-            most_bananas = std::max(most_bananas, bananas);
+            most_bananas = std::max(most_bananas, v);
         }
 
         return fmt::format("Most bananas you can get: {}", most_bananas);
